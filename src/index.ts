ba81ac7655a3fd4441b0e6a04e98c1f532317f47
sub0ne1 <iamsubhajit.d@gmail.com>
2024-05-31 00:56:09 -0400

    some

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,41 @@ import { Hono } from 'hono';
 
 const app = new Hono()
 
+type User = {
+  id: string,
+  name: string,
+  email: string,
+  mobile: string
+}
+
+type details = {
+  numSeats: number,
+  special?: string
+}
+
+type reservation = {
+  id: number,
+  user: User,
+  reservedFor: Date, // which date is that reseravation for
+  time: string,
+  createdAt: Date,
+  details: details, 
+}
+
+const reservations: reservation[] = [];
+
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.text('Welcome to yumyum API!')
+})
+
+app.get('/v1/reservations', (c) => {
+  return c.json(reservations)
+})
+
+app.get('/v1/reservations/:id', (c) => {
+  const { id } = c.req.param()
+  const reseravation = reservations.filter(data => data.id === parseInt(id))
+  return c.json(reseravation)
 })
 
 export default app;
